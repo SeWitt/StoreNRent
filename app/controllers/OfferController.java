8b42ad73a8b3a92f3c6463aa8fda92c1c46a1caf
SeWitt <sebi@witt-mann.de>
2014-06-03 18:06:42 +0200

    Offerform geaddet

--- app/controllers/OfferController.java
+++ app/controllers/OfferController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Offer;
 import play.api.templates.Html;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -17,18 +18,37 @@ import appinfo.GlobalValues;
  */
 public class OfferController extends Controller {
 
-	private OfferService offerService = new OfferServiceDummy();//if the backend is ready switch to "..Impl" instead of "..Dummy"
+	private static OfferService offerService = new OfferServiceDummy();//if the backend is ready switch to "..Impl" instead of "..Dummy"
 	private RecommendationService recommendationService = new RecommendationServiceDummy();//if the backend is ready switch to "..Impl" instead of "..Dummy"
 
 	public OfferController(){
 
 	}
 
-	public static Result index() {
+	public static Result index(Long id) {
 		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
-		String title = "Garage for rent";
-		Html content = views.html.offer.render(title, menubar);
+		Offer o = offerService.findByOfferID(id.longValue());
+		
+		Html content = views.html.offer.render(menubar,o);
         return ok(content);
     }
+	
+	public static Result edit(Long id) {
+		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
+		Offer o = offerService.findByOfferID(id.longValue());
+		
+		Html content = views.html.offerform.render(menubar,o);
+        return ok(content);
+    }
+	
+	public static Result create() {
+		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
+		Offer o = new Offer();
+		
+		Html content = views.html.offer.render(menubar,o);
+        return ok(content);
+    }
+
+	
 
 }
