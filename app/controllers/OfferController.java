81c020f40f25f31635790dec7e9a03b1bf5eac42
SeWitt <sebi@witt-mann.de>
2014-06-03 22:41:49 +0200

    Backend offerform created
    Offerform backend created -> no access to database -> play fuck
    TODO: call Jscript to get geo coordinates , Errormessages, Validation

--- app/controllers/OfferController.java
+++ app/controllers/OfferController.java
@@ -2,12 +2,14 @@ package controllers;
 
 import models.Offer;
 import play.api.templates.Html;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import service.OfferService;
 import service.RecommendationService;
 import serviceDummy.OfferServiceDummy;
 import serviceDummy.RecommendationServiceDummy;
+import serviceImpl.OfferServiceImpl;
 import appinfo.GlobalValues;
 
 
@@ -18,7 +20,7 @@ import appinfo.GlobalValues;
  */
 public class OfferController extends Controller {
 
-	private static OfferService offerService = new OfferServiceDummy();//if the backend is ready switch to "..Impl" instead of "..Dummy"
+	private static OfferService offerService = new OfferServiceImpl();//if the backend is ready switch to "..Impl" instead of "..Dummy"
 	private RecommendationService recommendationService = new RecommendationServiceDummy();//if the backend is ready switch to "..Impl" instead of "..Dummy"
 
 	public OfferController(){
@@ -33,20 +35,71 @@ public class OfferController extends Controller {
         return ok(content);
     }
 	
+	public static Result newOffer(){
+		
+		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
+		Form<Offer> offerForm = Form.form(Offer.class);	
+		
+		Html content = views.html.offerform.render(offerForm,menubar);
+        return ok(content);
+		
+		
+	}
+	
+	
 	public static Result edit(Long id) {
+
 		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
 		Offer o = offerService.findByOfferID(id.longValue());
-		
-		Html content = views.html.offerform.render(menubar,o);
+			
+		Form<Offer> offerForm = Form.form(Offer.class);
+		offerForm = offerForm.fill(o);
+			
+		Html content = views.html.offerform.render(offerForm,menubar);
         return ok(content);
     }
 	
 	public static Result create() {
-		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
-		Offer o = new Offer();
 		
-		Html content = views.html.offer.render(menubar,o);
-        return ok(content);
+		Form<Offer> offerForm = Form.form(Offer.class).bindFromRequest();
+		
+		String[] postAction =request().body().asFormUrlEncoded().get("action");
+		
+		Result result = TODO;
+		
+		if (postAction == null || postAction.length == 0) {
+			result = redirect(routes.HomeController.index());
+			System.out.println("postaction null");
+		}else{
+			String action = postAction[0];
+			
+			if(action.equals("submit")){
+//					if(offerForm.hasErrors()){
+//				Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
+//					return badRequest(views.html.offerform.render(offerForm, menubar)); 
+//				}else{
+					Offer o = offerForm.get();
+					o = offerService.createOffer(o);
+					System.out.println("offer id:" +o.id);
+					flash("success", "Successfully created!");
+				
+					System.out.println("submit pressed");
+
+					result = redirect(routes.OfferController.index(o.id));
+//				}
+			}else if(action.equals("discard")){
+				result = redirect(routes.HomeController.index());
+				System.out.println("discard pressed");
+			}
+		}
+		return result;
+		
+		
+		
+			
+
+		
+		
     }
 
 	
