d3c1f57a95f6d626dd3a3d44d69bfa756e6f1135
SeWitt <sebi@witt-mann.de>
2014-06-05 20:26:16 +0200

    delete unused code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,18 +1,9 @@
 package controllers;
 
 
-import java.util.List;
-
-
-import models.Offer;
-import models.Person;
-import models.SearchAttributes;
 import play.Routes;
-import play.mvc.*;
-import service.OfferService;
-import serviceDummy.OfferServiceDummy;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 
 public class Application extends Controller {
@@ -28,48 +19,6 @@ public class Application extends Controller {
 	                                      routes.javascript.SearchController.query()));
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	private static OfferService offerService = new OfferServiceDummy();
-	
-	
-//    public static Result index() {
-//    	
-//    	 response().setContentType("text/html");
-//    	 
-//    	 SearchAttributes s = new SearchAttributes();
-//    	 
-//    	return ok(search.render("Munich", searchmap.render(),null, searchinput.render(s)));
-////        return ok(index.render("Saudeppertes Play."));
-//    }
-//    
-//    
-//    
-//    public static Result searchInput(){
-//    	 SearchAttributes s = new SearchAttributes();
-//    	return ok(searchinput.render(s));
-//    }
-//    
-//    
-//    public static Result searchResults(){
-//    	
-//    	Person p = new Person();
-//    	p.id = 2;
-//    	p.lastName= "we";
-//    	p.surname ="dd";
-//    	List<Offer> o = offerService.findByOwnerID(p);
-//    	return ok(searchresults.render(o)) ;
-//    }
-    
-    
    
     
 
