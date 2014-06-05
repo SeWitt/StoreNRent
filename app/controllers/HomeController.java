5406999f46264fcea9a13a945a449567a09e3302
SeWitt <sebi@witt-mann.de>
2014-06-06 01:46:18 +0200

    initial commit homepage
    Anlegen der Hompage. Funktionalität ist da, Layout / Design fehlt noch

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,12 +1,21 @@
 package controllers;
+import java.util.List;
+
+import models.HomePageSearchForm;
+import models.Offer;
+import models.Person;
+import models.SearchAttributes;
+import play.api.templates.Html;
+import play.data.Form;
 import play.mvc.Controller;
+import play.mvc.Result;
 import service.DiscoveryService;
 import service.NewsService;
+import service.OfferService;
 import serviceDummy.DiscoveryServiceDummy;
 import serviceDummy.NewsServiceDummy;
-
-
-import play.mvc.*;
+import serviceDummy.OfferServiceDummy;
+import appinfo.GlobalValues;
 /**
  * @author Sebastian
  * @version 1.0
@@ -15,12 +24,67 @@ import play.mvc.*;
 public class HomeController extends Controller {
 
 	private NewsService newsService = new NewsServiceDummy();//if the backend is ready switch to "..Impl" instead of "..Dummy"
-	private DiscoveryService discoveryService =  new DiscoveryServiceDummy();//if the backend is ready switch to "..Impl" instead of "..Dummy"
-
+	private static DiscoveryService discoveryService =  new DiscoveryServiceDummy();//if the backend is ready switch to "..Impl" instead of "..Dummy"
+	private static OfferService offerService = new OfferServiceDummy();
 
 		
 	public static Result index(){
-		return TODO;
+		//menue bar
+		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
+		
+		Form<HomePageSearchForm> searchForm = Form.form(HomePageSearchForm.class);
+		
+		
+		Html content = views.html.home.render(searchForm,menubar);
+        return ok(content);
+	}
+	
+	
+	public static Result search(){
+		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
+		Form<HomePageSearchForm> searchForm = Form.form(HomePageSearchForm.class).bindFromRequest();
+		HomePageSearchForm hpsf = searchForm.get();
+		String[] postAction =request().body().asFormUrlEncoded().get("action");
+		
+		Result result = TODO;
+		
+		if (postAction == null || postAction.length == 0) {
+			result = redirect(routes.HomeController.index());
+		}else{
+			String action = postAction[0];
+			
+			if(action.equals("submit")){
+				
+					String city = null;
+					Double spacesize =null;
+					SearchAttributes sa = new SearchAttributes();
+					List<Offer> offers = null;
+					
+					
+					if(hpsf.city != null){
+						city = hpsf.city;
+						spacesize =hpsf.spacesize;
+						
+						sa.city = city;
+						sa.spaceSize = spacesize;
+						//TODO: Replace through discovery service (real query, no dummy!)
+						Person p = new Person();
+				    	p.id = 2;
+				    	p.lastName= "we";
+				    	p.surname ="dd";
+				    	offers = offerService.findByOwnerID(p);
+					
+				    	//TODO: Discovery service implementieren
+//						offers = discoveryService.findOffers(sa);
+					}
+					
+					result = ok(views.html.search.render(city, spacesize, offers, menubar));
+
+			}else {
+				result = redirect(routes.HomeController.index());
+			}
+		}
+		return result;
 	}
 
 
