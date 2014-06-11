ee85b5c473a5d581ff58f39812a86cc236a9d349
Marius Popp <mpopp@outlook.com>
2014-06-11 14:59:47 +0200

    Menubar, Offer edited
    Menubar:
    + redundant html-code deleted
    
    Offer:
    + 'Offer last edited' added

--- app/serviceDummy/OfferServiceDummy.java
+++ app/serviceDummy/OfferServiceDummy.java
@@ -2,13 +2,13 @@ package serviceDummy;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
 
 import models.Offer;
 import models.Person;
 import models.PersonSettings;
-import repository.PersonRepository;
 import service.OfferService;
 import service.PersonalSettingsService;
 
@@ -58,7 +58,11 @@ public class OfferServiceDummy implements OfferService {
 		offer.country = "Germany";
 		offer.geolocX = 48.138914;
 		offer.geolocY = 11.580177;
-		offer.createdDate = new Timestamp(System.currentTimeMillis());
+		
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.add(GregorianCalendar.DATE, -25);
+		offer.createdDate = new Timestamp(cal.getTimeInMillis());
+		
 		offer.description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.<br><br> Sed diam "
 				+ "nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed "
 				+ "diam voluptua. <br><br>At vero eos et accusam et justo duo dolores et ea rebum. Stet "
