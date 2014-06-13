73e8d0c5f21c5411b87972e92d6a354de25f5d7a
SeWitt <sebi@witt-mann.de>
2014-06-13 21:50:54 +0200

    Little code cleaning

--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -12,16 +12,12 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 import models.Offer;
-import models.Person;
 import models.SearchAttributes;
 import play.api.templates.Html;
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import service.DiscoveryService;
-import service.OfferService;
-import serviceDummy.DiscoveryServiceDummy;
-import serviceDummy.OfferServiceDummy;
 import serviceImpl.DiscoveryServiceImpl;
 import views.html.search;
 import views.html.searchresults;
@@ -39,32 +35,48 @@ import appinfo.GlobalValues;
 public class SearchController extends Controller {
 
 	private static DiscoveryService discoveryService = new DiscoveryServiceImpl();//if the backend is ready switch to "..Impl" instead of "..Dummy"
-	private static OfferService offerService = new OfferServiceDummy();
+//	private static OfferService offerService = new OfferServiceDummy();
 
+	/**Method for the home site search
+	 * 
+	 * @param city city parameter
+	 * @param radius radius
+	 * @return a rendered html file
+	 */
 	@Transactional
-	public static Result search(String city, Double spacesize){
+	public static Result search(String city, Double radius){
 		Html menubar = views.html.menubar.render(GlobalValues.NAVBAR_SEARCH);
-		if(city == null && spacesize == null){
-			return ok(search.render(city, spacesize, null, menubar));
+		if(city == null && radius == null){
+			return ok(search.render(city, radius, null, menubar));
 		}else{
-			
-//			Person p = new Person();
-//	    	p.id = 2;
-//	    	p.lastName= "we";
-//	    	p.surname ="dd";
-//	    	List<Offer> o = offerService.findByOwnerID(p);
 			
 			SearchAttributes sa = new SearchAttributes();
 			sa.city = city;
-			sa.spaceSize = spacesize;
-			
+			sa.spaceSize = radius;
+			//find offers
 			List<Offer> o = discoveryService.findOffers(sa);
 	
-			
-			return ok(search.render(city, spacesize,o , menubar));
+			return ok(search.render(city, radius,o , menubar));
 		}
 		
 	}
+	
+	/**search method for search site search
+	 * 
+	 * Parameters:
+	 * encapsulates all in an Searchattributes
+	 * 
+	 * @param fromdate
+	 * @param todate
+	 * @param city
+	 * @param postcode
+	 * @param spacesize
+	 * @param maxprice
+	 * @param radius
+	 * @param lng
+	 * @param lat
+	 * @return
+	 */
 	@Transactional
 	public static Result query(String fromdate, String todate, String city, String postcode, Double spacesize, Double maxprice, Double radius, String lng, String lat){
 		
@@ -117,15 +129,7 @@ public class SearchController extends Controller {
 			sa.lng = longitude;
 			sa.lat = latitude;
 		} catch (Exception e) {}
-//Just DEBUGGING, DUMMY		
-//		Person p = new Person();
-//    	p.id = 2;
-//    	p.lastName= "we";
-//    	p.surname ="dd";
-//    	List<Offer> o = offerService.findByOwnerID(p);
-//		o.remove(0);
-//		System.out.println("ja ich wurde angesprochen");
-		 
+
 //PRODUCTIVE USE		
 		List<Offer> o = discoveryService.findOffers(sa);
 		
